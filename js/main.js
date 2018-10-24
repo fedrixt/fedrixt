3ce551d190d921e2cd81efabbbd6168fe688213e
fedrixt <fedrixt@gmail.com>
2018-10-24 15:34:00 +0300

    ui.js deleted

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+	setTimeout(function () {
+		$("#loader-img img").fadeOut(3000)
+	}, 0);
+	setTimeout(function () {
+		$("#loader-img").fadeOut(3000)
+	}, 0);
+});
--- js/ui-min.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-	setTimeout(function () {
-		$("#loader-img img").fadeOut(3000)
-	}, 0);
-	setTimeout(function () {
-		$("#loader-img").fadeOut(3000)
-	}, 0);
-});
-
