9651e474e771c23d3a856a2c1abb3e77a193d6ec
fedrixt <fedrixt@gmail.com>
2018-10-25 15:04:16 +0300

    go-top button created

--- js/main.js
+++ js/main.js
@@ -1,8 +1,26 @@
 $(document).ready(function () {
+// loader
 	setTimeout(function () {
 		$("#loader-img img").fadeOut(3000)
 	}, 0);
 	setTimeout(function () {
 		$("#loader-img").fadeOut(3000)
 	}, 0);
+
+// go-top button
+	var goTopButton = function () {
+		var goButtonShow = 500,
+			fadeInTime = 400,
+			fadeOutTime = 400,
+			scrollSpeed = 400,
+			goTop = $("#go-top");
+		$(window).on('scroll', function () {
+			if ($(window).scrollTop() >= goButtonShow) {
+				goTop.fadeIn(fadeInTime);
+			} else {
+				goTop.fadeOut(fadeOutTime);
+			}
+		});
+	};
+	goTopButton();
 });
